38f67760b1333f613405ad96348651649c8a2540
Matheus Dóros Costa <matheus.ti.dev@outlook.com>
2023-07-05 13:22:05 -0300

    Adicionar arquivos de projeto.

---
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using System.Diagnostics;
+using SalesWebMvc.Models.ViewModels;
+
+namespace SalesWebMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
